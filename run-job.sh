dc636ae278f8f96fceb8e9d229e70a9c9130cbc7
Manuel Padilla <manuelpad@gmail.com>
2025-08-10 17:24:17 +0200

    fix: install all dependencies for tsx compatibility

--- run-job.sh
+++ run-job.sh
@@ -3,6 +3,6 @@ cd /home/ratsweb/source/MAYZ/mayz-mainnet-incentivized-program-job
 # If you need to pull latest changes:
 git pull
 # Run your job (replace with your actual command, e.g. node, ts-node, etc.)
-npm install --production
+npm install 
 # npx ts-node src/index.ts
 npm run start
